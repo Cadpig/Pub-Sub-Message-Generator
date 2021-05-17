b9221dd75e98e60521c56a51373c91dcc97469c7
Владислав Тихонов <vladislavtihonov@MacBook-Pro-Vladislav.local>
2021-05-17 22:57:07 +0400

    Исправлены ошибки

--- Main.py
+++ Main.py
@@ -10,26 +10,39 @@ topic_id = "pub_sub_messages"
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_id)
 
-for n in range(1, 40):
-    #data = "Message number {}".format(n)
-    country = ["US", "GB", "RQ", "VI", "GU"]
-    crand = random.randint(0,4)
-    data = ""
-    data = data + country[crand]
-    data = data + ","
-    type_aircraft = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    trand = random.randint(0,8)
-    data = data + type_aircraft[trand]
-    data = data + ","
-    year_mfr = ["1936", "1942", "1953", "1964", "1975", "1986", "1997", "2008", "2019"]
-    yrand = random.randint(0,8)
-    data = data + year_mfr[yrand]
+message = "US,1,1936"
+message = message.encode("utf-8")
+msg = "GB,2,1955"
+msg = msg.encode("utf-8")
+for n in range(1, 10):
+   future = publisher.publish(topic_path, message)
+   future = publisher.publish(topic_path, message)
+   future = publisher.publish(topic_path, msg)
+   time.sleep(2)
 
-    # Data must be a bytestring
-    data = data.encode("utf-8")
-    # When you publish a message, the client returns a future.
-    future = publisher.publish(topic_path, data)
-    print(future.result())
-    time.sleep(2)
+
+#for n in range(1, 40):
+while 1:
+   #data = "Message number {}".format(n)
+   country = ["US", "GB", "RQ", "VI", "GU"]
+   crand = random.randint(0, 4)
+   data = ""
+   data = data + country[crand]
+   data = data + ","
+   type_aircraft = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+   trand = random.randint(0, 8)
+   data = data + type_aircraft[trand]
+   data = data + ","
+   year_mfr = ["1936", "1942", "1953", "1964", "1975", "1986", "1997", "2008", "2019"]
+   yrand = random.randint(0, 8)
+   data = data + year_mfr[yrand]
+
+   # Data must be a bytestring
+   data = data.encode("utf-8")
+   # When you publish a message, the client returns a future.
+   future = publisher.publish(topic_path, data)
+   print(future.result())
+   time.sleep(2)
+
 
 print(f"Published messages to {topic_path}.")
